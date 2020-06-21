7fc000be26144fef7ee9eadd283e0a248b322025
Katherine Winter <katherine.m.winter@gmail.com>
2020-06-21 19:49:11 -0400

    fix crash in completion data

--- src/utils/completion-date-info.js
+++ src/utils/completion-date-info.js
@@ -1,6 +1,6 @@
 export function completionDateInfo(userData, step) {
-  const finishedStep = userData?.find((finished) => finished.id === step.id)
-  const completionDate = new Date(finishedStep?.completionDate)
+  const finishedStep = userData?.find((finished) => finished.id === step?.id)
+  const completionDate = finishedStep ? new Date(finishedStep?.completionDate) : null
   return {
     isDateValid: !isNaN(completionDate),
     date: completionDate,
